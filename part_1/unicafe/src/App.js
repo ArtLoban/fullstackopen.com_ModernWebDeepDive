bb6d85f257ecdba64a378fd1199afd03533eee87
Artem <artem.loban@devit-team.com>
2023-04-15 19:19:28 +0300

    Task 1.7 - Step2

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -8,13 +8,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total * 100);
+
   return (
     <div>
       <h1>Give feedback</h1>
       <div>
-        <Button text={'Good'} handleClick={() => setGood(good+1)} />
-        <Button text={'Neutral'} handleClick={() => setNeutral(neutral+1)} />
-        <Button text={'Bad'} handleClick={() => setBad(bad+1)} />
+        <Button text="Good" handleClick={() => setGood(good+1)} />
+        <Button text="Neutral" handleClick={() => setNeutral(neutral+1)} />
+        <Button text="Bad" handleClick={() => setBad(bad+1)} />
       </div>
 
       <h2>Statistics</h2>
@@ -22,6 +26,10 @@ const App = () => {
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <hr/>
+        <p>All: {total}</p>
+        <p>Average: {isNaN(average) ? 0 : average}</p>
+        <p>Positive: {isNaN(positive) ? 0 : positive}%</p>
       </div>
     </div>
   )
