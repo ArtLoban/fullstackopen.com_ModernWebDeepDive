07138da2544a336982d70e90d1e37ab7450c160d
Artem <artem.loban@devit-team.com>
2023-04-16 04:21:36 +0300

    Task 2.5

--- part_2/courseinfo/src/components/App.js
+++ part_2/courseinfo/src/components/App.js
@@ -1,42 +1,4 @@
-const Part = ({ name, exercises}) => {
-  return <p>{name} {exercises}</p>
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Header = ({ title }) => {
-  return (
-    <h1>{title}</h1>
-  )
-}
-
-const Footer = ({ parts }) => {
-  const total = parts.reduce((accumulator, part) => {
-      return accumulator + part.exercises
-    }, 0)
-
-  return (
-    <div>
-      <b>total of {total} exercises</b>
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header title={course.name} />
-      <Content parts={course.parts} />
-      <Footer parts={course.parts} />
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
