b20e2fa7a584934c3b0f1aacdabb7acd6bdf5329
Artem <artem.loban@devit-team.com>
2023-04-20 21:52:23 +0300

    Task 3.5 - Step5

--- part_3/phonebook-back/index.js
+++ part_3/phonebook-back/index.js
@@ -71,6 +71,36 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body      // Without the json-parser, the body property would be undefined.
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'Name is required!'
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'Phone number is required!'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
