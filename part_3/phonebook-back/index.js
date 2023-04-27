4de5f3ae8c1d57b84bd240bdd59202f9a83614e2
Artem <artem.loban@devit-team.com>
2023-04-27 10:37:15 +0300

    Task 3.17 - Step5

--- part_3/phonebook-back/index.js
+++ part_3/phonebook-back/index.js
@@ -119,6 +119,22 @@ app.post('/api/persons', (request, response) => {
     .catch(error => next(error))
 })
 
+// UPDATE Person
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
