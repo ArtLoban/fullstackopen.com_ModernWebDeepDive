556309f7c03f6883a8896b9839330888d43fa1df
Artem <artem.loban@devit-team.com>
2023-04-28 12:41:26 +0300

    Task 4.1 - Step1

--- part_4/blog-list/index.js
+++ part_4/blog-list/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -12,7 +13,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -20,23 +21,23 @@ app.use(express.json())
 
 app.get('/api/blogs', (request, response) => {
   Blog
-  .find({})
-  .then(blogs => {
-    response.json(blogs)
-  })
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
 })
 
 app.post('/api/blogs', (request, response) => {
   const blog = new Blog(request.body)
 
   blog
-  .save()
-  .then(result => {
-    response.status(201).json(result)
-  })
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
