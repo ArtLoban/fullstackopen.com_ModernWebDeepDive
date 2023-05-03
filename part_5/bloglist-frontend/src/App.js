540ddb3d3e9275a35805811a460b44155c2f4db2
Artem <artem.loban@devit-team.com>
2023-05-03 14:07:03 +0300

    Task 5.1 - Step1

---
+++ part_5/bloglist-frontend/src/App.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import LoginForm from './components/LoginForm';
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs => setBlogs(blogs))
+  }, [])
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <LoginForm setUser={setUser} />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog => <Blog key={blog.id} blog={blog} /> )}
+    </div>
+  )
+}
+
+export default App
---
+++ part_5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import loginService from '../services/login';
+
+const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({username, password})
+
+      if (typeof user.token !== 'undefined') {
+        setUser(user)
+        setUsername('')
+        setPassword('')
+      }
+      // todo: show validation error
+    } catch (e) {
+      console.log(`Caught error: ${e.message}`);
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}
+
+export default LoginForm
