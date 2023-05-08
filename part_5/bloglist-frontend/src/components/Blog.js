178f7f522e13e842718af0951eef5692baa262db
Artem <artem.loban@devit-team.com>
2023-05-08 18:05:15 +0300

    Task 5.7 - Step7

--- part_5/bloglist-frontend/src/components/Blog.js
+++ part_5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, toggleVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button
+          onClick={() => toggleVisible(!visible)}
+          style={{marginLeft: '10px'}}
+          type="button"
+        >{visible ? 'Hide' : 'View'}</button>
+      </div>
+      {visible &&
+        <div>
+          <span>{blog.url}</span>
+          <div>
+            <span>likes {blog.likes}</span>
+            <button type="button" style={{marginLeft: '10px'}}>Like</button>
+          </div>
+          <span>{blog.author}</span>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
