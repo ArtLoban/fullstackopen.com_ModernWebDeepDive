b3637dc47a675d415f437a1654b45a2ab38100b8
Artem <artem.loban@devit-team.com>
2023-05-25 15:07:17 +0300

    Task 6.3 - Step1

--- part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,19 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch(action.type) {
+    case 'VOTE':
+      const id = action.payload.id
+      const itemToVote = state.find(item => item.id === id)
+      const changedItem = {
+        ...itemToVote,
+        votes: ++itemToVote.votes
+      }
 
-  return state
+      return state.map(item => item.id !== id ? item : changedItem)
+    default:
+      return state
+  }
 }
 
 export default reducer
