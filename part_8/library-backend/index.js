4922a3a821e03b8865b14a1c14064de598469617
Artem <artem.loban@devit-team.com>
2023-06-09 16:42:29 +0300

    8.3 - All authors

--- part_8/library-backend/index.js
+++ part_8/library-backend/index.js
@@ -92,10 +92,17 @@ const typeDefs = `
     genres: [String]!
   }
   
+  type Author {
+    name: String
+    born: Int
+    bookCount: Int!
+  }
+  
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -103,8 +110,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
   },
+  Author: {
+    bookCount: (root) => books.filter(book => book.author === root.name).length
+  }
 }
 
 const server = new ApolloServer({
