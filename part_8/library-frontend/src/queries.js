3273000e0a95a8385bdac4faed2ea9ec60918f8a
Artem <artem.loban@devit-team.com>
2023-06-12 13:30:33 +0300

    Task 8.8 - Authors view

---
+++ part_8/library-frontend/src/queries.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+    query {
+        allAuthors {
+            id
+            name
+            born
+            bookCount
+        }
+    }
+`
+
