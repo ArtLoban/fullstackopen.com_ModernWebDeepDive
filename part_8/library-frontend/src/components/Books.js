b5172e0ae79f9b0abeb4c65d2d712c988f97fcce
Artem <artem.loban@devit-team.com>
2023-06-19 14:56:19 +0300

    Task 8.17 - Listing books

--- part_8/library-frontend/src/components/Books.js
+++ part_8/library-frontend/src/components/Books.js
@@ -14,26 +14,37 @@ const Books = (props) => {
 
   const books = result?.data?.allBooks || []
 
+  console.log(books.length);
+
   return (
     <div>
       <h2>books</h2>
 
-      <table>
-        <tbody>
+      { books.length > 0
+        ?
+        <table>
+          <tbody>
           <tr>
-            <th></th>
+            <th>Name</th>
             <th>author</th>
             <th>published</th>
           </tr>
           {books.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
-              <td>{a.author}</td>
+              <td>{a.author?.name}</td>
               <td>{a.published}</td>
             </tr>
           ))}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+        :
+        <p>No data</p>
+      }
+
+
+
+
     </div>
   )
 }
